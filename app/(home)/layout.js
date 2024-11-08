16890d8249c017965616b6e16f7e0f4692d180ba
Niyoj Oli <oliniyoj@gmail.com>
2024-11-08 08:15:15 +0545

    makes navbar dynamic

--- app/(home)/layout.js
+++ app/(home)/layout.js
@@ -1,13 +1,18 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import { Navbar } from "@/app/_features";
 
 import styles from "./layout.module.css";
 
 export default function HomepageLayout({ children }) {
+  const pathname = usePathname().slice(1);    // removes the first slash character
+
   return (
     <>
       {children}
       <div className={styles["navbar__wrapper"]}>
-        <Navbar />
+        <Navbar active={pathname} />
       </div>
     </>
   );
--- app/zen/page.js
+++ app/zen/page.js
@@ -1,3 +1,18 @@
+import { Volume2, ZapOff } from "lucide-react";
+
+import styles from "./zen.module.css";
+
 export default function ZenPage() {
-  return <p>ZenMode</p>;
+  return (
+    <>
+      <button className={styles["sound_icon__wrapper"]}>
+        <Volume2 />
+      </button>
+
+      <button className={styles["exit_btn"]}>
+        <ZapOff />
+        <small>Exit Zen Mode</small>
+      </button>
+    </>
+  );
 }
