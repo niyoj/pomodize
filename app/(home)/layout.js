c57dc864b61d89fa29cc46ac90f88b3810eca4fa
Niyoj Oli <oliniyoj@gmail.com>
2024-11-08 19:56:22 +0545

    improves layout of homepage

--- app/(home)/layout.js
+++ app/(home)/layout.js
@@ -1,19 +1,27 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { Zap } from "lucide-react";
 import { Navbar } from "@/app/_features";
 
 import styles from "./layout.module.css";
 
 export default function HomepageLayout({ children }) {
-  const pathname = usePathname().slice(1);    // removes the first slash character
+  const pathname = usePathname().slice(1); // removes the first slash character
 
   return (
-    <>
-      {children}
+    <div className={styles["homepage__wrapper"]}>
+      <main className={styles["homepage__content"]}>
+        {children}
+
+        <div className={styles["zen_fab"]}>
+          <Zap size="2rem" />
+        </div>
+      </main>
+
       <div className={styles["navbar__wrapper"]}>
         <Navbar active={pathname} />
       </div>
-    </>
+    </div>
   );
 }
