15afee612d3c7c7e20ae9635a79c4ffb06cbbacd
Niyoj Oli <oliniyoj@gmail.com>
2024-11-10 00:45:47 +0545

    completes pomodoro page details section

--- app/_features/pomodoro-details/pomodoro-details.js
+++ app/_features/pomodoro-details/pomodoro-details.js
@@ -1,3 +1,10 @@
+"use client";
+
+import {
+  cyclePomodoroType,
+  getIntervalPercentage,
+  pomodoroLength,
+} from "@/app/_lib";
 import styles from "./pomodoro-details.module.css";
 
 const pomodoroSessionInfo = [
@@ -6,11 +13,30 @@ const pomodoroSessionInfo = [
   { name: "longBreak", display: "long break", color: "#8E0202" },
 ];
 
-export function PomodoroDetails() {
+export function PomodoroDetails({
+  now,
+  next,
+  cycle,
+  currPomoStack,
+  percentage,
+}) {
+  const tabsPercentage = getIntervalPercentage();
+
+  const getColor = (name) =>
+    pomodoroSessionInfo.find((item) => item.name === name).color;
+
+  const getDisplayName = (name) =>
+    pomodoroSessionInfo.find((item) => item.name === name).display;
+
   return (
     <section className={styles["details"]}>
-      <p>Pomodoro #1</p>
-      <small>Focus Time - 25 mins.</small>
+      <p>Pomodoro #{cycle}</p>
+      <small>
+        <span style={{ textTransform: "capitalize" }}>
+          {getDisplayName(now)}
+        </span>
+        - {pomodoroLength[now]} mins.
+      </small>
 
       <div className={styles["details__bar__wrapper"]}>
         <small>
@@ -18,11 +44,35 @@ export function PomodoroDetails() {
           <span
             style={{ color: "var(--color-neutral-700)", marginLeft: "4px" }}
           >
-            short break
+            {getDisplayName(next)}
           </span>
         </small>
 
-        <div className={styles["details__bar"]}></div>
+        <div className={styles["details__bar"]}>
+          {currPomoStack
+            .slice(0, currPomoStack.length - 1)
+            .map((item, index) => (
+              <div
+                key={index}
+                style={{ width: `${tabsPercentage[item]}%` }}
+                className={`${styles["details__bar__tab"]} ${styles[`details__bar__tab--${item}`]}`}
+              >
+                <div style={{ backgroundColor: getColor(item) }} />
+              </div>
+            ))}
+
+          <div
+            style={{ width: `${tabsPercentage[currPomoStack.at(-1)]}%` }}
+            className={`${styles["details__bar__tab"]} ${styles[`details__bar__tab--${currPomoStack.at(-1)}`]}`}
+          >
+            <div
+              style={{
+                width: `${percentage}%`,
+                backgroundColor: getColor(currPomoStack.at(-1)),
+              }}
+            />
+          </div>
+        </div>
 
         <div className={styles["details__legends"]}>
           {pomodoroSessionInfo.map((item) => (
